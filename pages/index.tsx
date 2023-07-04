d19f224a5bd5cb474d5e344b5ffeb1796d740eb1
rathan2 <skugarathan2@gmail.com>
2023-07-04 22:16:48 +0530

    main

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Travel booking!</h1>
+      <p>This is a Next.js app with TypeScript .</p>
+    </div>
+  );
+};
+
+export default Home;
