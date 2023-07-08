80c38848854492d8cd2ba9e54fac86be6cc1c4e9
rathan2 <skugarathan2@gmail.com>
2023-07-09 01:12:58 +0530

    index 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,34 @@ const Home: React.FC = () => {
         <div className="image_overlay">
           <div className="main_text">
             <h1 className="main_h1 right_align_center">
-            The Ultimate Secure  Reservation Experience
+              The Ultimate Secure Reservation Experience
             </h1>
-            <h4 className="main_h3">Your Seat, Your Choice: Anywhere, Anytime</h4>
+            <h4 className="main_h3">
+              Your Seat, Your Choice: Anywhere, Anytime
+            </h4>
           </div>
         </div>
         <img className="main_image" src="image_m.jpg" />
       </div>
-      <div className="main_form_content">
-         <h3 className="main_form_h3">Choose Your Journey</h3>
+      <div className="main_form">
+        <div className="title">
+          <h3 className="main_form_h3">Choose Your Journey</h3>
+        </div>
+        <div className="main_form_content">
+          <div className="from">
+            <input placeholder="From" />
+          </div>
+          <div className="to">
+            <input placeholder="To" />
+          </div>
+          <div className="depature">
+            <input placeholder="Depature" />
+          </div>
+          <div className="seats">
+            <input placeholder="Seats" />
+          </div>
+          <button>Check Availabilty</button>
+        </div>
       </div>
     </div>
   );
