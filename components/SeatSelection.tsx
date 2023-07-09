bd1766fd1bda137c5ffab9b0c70ccb642ba951c9
rathan2 <skugarathan2@gmail.com>
2023-07-09 11:00:44 +0530

    seatselection2

--- components/SeatSelection.tsx
+++ components/SeatSelection.tsx
@@ -6,10 +6,15 @@ const SeatSelection: React.FC = () => {
   const rows = 20;
   const seatsPerRow = 4;
 
+  const preselectedSeats = ['2-1', '5-3']; // Add your preselected seats here
+
   const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
 
-  const handleSeatSelection = (row: number, seat: number) => {
-    const seatId = `${row}-${seat}`;
+  const handleSeatSelection = (seatId: string) => {
+    // Check if the seat is preselected or not before handling the selection
+    if (preselectedSeats.includes(seatId)) {
+      return;
+    }
 
     // Toggle seat selection
     setSelectedSeats((prevSelectedSeats) => {
@@ -24,27 +29,30 @@ const SeatSelection: React.FC = () => {
   const renderSeatSelection = () => {
     const seatSelection: JSX.Element[] = [];
 
-    for (let row =0; row <= rows-1; row++) {
+    for (let row = 1; row <= rows; row++) {
       const seats: JSX.Element[] = [];
 
       for (let seat = 1; seat <= seatsPerRow; seat++) {
         const seatId = `${row}-${seat}`;
 
+        const isPreselected = preselectedSeats.includes(seatId);
+        const isChecked = selectedSeats.includes(seatId);
+
         seats.push(
           <label key={`seat-${row}-${seat}`} className="seat-label">
             <input
               type="checkbox"
-              checked={selectedSeats.includes(seatId)}
-              onChange={() => handleSeatSelection(row, seat)}
+              checked={isChecked}
+              disabled={isPreselected}
+              onChange={() => handleSeatSelection(seatId)}
             />
-            <span className="seat-number">Seat {seat}</span>
+            {seat}
           </label>
         );
       }
 
       seatSelection.push(
         <div key={`row-${row}`} className="row-wrapper">
-          <span className="row-label">Row {row}</span>
           <div className="seat-wrapper">{seats}</div>
         </div>
       );
